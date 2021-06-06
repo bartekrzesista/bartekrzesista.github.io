a191c6be7b8af9a28d4f3c48a2433e2cddcab46e
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-06-06 21:17:51 +0200

    add fetching repos from github

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,51 @@
 import '../scss/main.scss';
 
 console.log('HELLO 🚀');
+
+fetch("https://api.github.com/users/bartekrzesista/repos?sort=created")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    const container = document.querySelector(".projects__grid--js");
+    for (let repo of resp) {
+      const { name, description, homepage, html_url } = repo;
+
+      const template = `<article class="repo">
+            <div class="repo__top">
+              <span class="repo__top-circle"></span>
+              <span class="repo__top-circle"></span>
+              <span class="repo__top-circle"></span>
+            </div>
+            <div class="repo__content">
+              <img
+                class="repo__github-img"
+                src="img/github-white.svg"
+                alt="Github logo"
+              />
+              <ul class="repo__list">
+                <li class="repo__list-item">
+                  <span class="repo__label">project:</span
+                  ><span class="repo__value repo__value-name">${name}</span>
+                </li>
+                <li class="repo__list-item">
+                  <span class="repo__label">description:</span
+                  ><span class="repo__value repo__value-description">${description}</span>
+                </li>
+                <li class="repo__list-item">
+                  <span class="repo__label">demo:</span
+                  ><span class="repo__value repo__value-link"><<a href="${homepage}">see here</a>></span>
+                </li>
+                <li class="repo__list-item">
+                  <span class="repo__label">github:</span
+                  ><span class="repo__value repo__value-link"><<a href="${html_url}">source code</a>></span>
+                </li>
+              </ul>
+            </div>
+          </article>`;
+
+      if(description) container.innerHTML += template;
+
+    }
+  })
+  .catch((e) => {
+    console.log("Failed to fetch");
+  });
