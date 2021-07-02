631add79564cc36681cea98ce1a32ce1cb99e920
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-07-02 16:38:01 +0200

    add hand animation and scroll behaviour, correct repo links

--- src/js/index.js
+++ src/js/index.js
@@ -10,37 +10,51 @@ fetch("https://api.github.com/users/bartekrzesista/repos?sort=created")
       const { name, description, homepage, html_url } = repo;
 
       const template = `<article class="repo">
-            <div class="repo__top">
-              <span class="repo__top-circle"></span>
-              <span class="repo__top-circle"></span>
-              <span class="repo__top-circle"></span>
-            </div>
-            <div class="repo__content">
-              <img
-                class="repo__github-img"
-                src="img/github-white.svg"
-                alt="Github logo"
-              />
-              <ul class="repo__list">
-                <li class="repo__list-item">
-                  <span class="repo__label">project:</span
-                  ><span class="repo__value repo__value-name">${name}</span>
-                </li>
-                <li class="repo__list-item">
-                  <span class="repo__label">description:</span
-                  ><span class="repo__value repo__value-description">${description}</span>
-                </li>
-                <li class="repo__list-item">
-                  <span class="repo__label">demo:</span
-                  ><span class="repo__value repo__value-link"><<a href="${homepage}">see here</a>></span>
-                </li>
-                <li class="repo__list-item">
-                  <span class="repo__label">github:</span
-                  ><span class="repo__value repo__value-link"><<a href="${html_url}">source code</a>></span>
-                </li>
-              </ul>
-            </div>
-          </article>`;
+      <div class="repo__top">
+        <span class="repo__top-circle"></span>
+        <span class="repo__top-circle"></span>
+        <span class="repo__top-circle"></span>
+      </div>
+      <div class="repo__content">
+        <img
+          class="repo__github-img"
+          src="img/github-white.svg"
+          alt="Github logo"
+        />
+        <ul class="repo__list">
+          <li class="repo__list-item">
+            <span class="repo__label">project:</span>
+            <span class="repo__value repo__value-name">${name}</span>
+          </li>
+          <li class="repo__list-item">
+            <span class="repo__label">description:</span>
+            <span class="repo__value repo__value-description">${description}</span>
+          </li>
+          <li class="repo__list-item">
+            <span class="repo__label">demo:</span>
+            <span class="repo__value repo__value-link">
+              &lt;<a
+                title="${name} - demo"
+                href="${homepage}"
+                target="_blank"
+                rel="noopener noreferrer"
+              >see here</a>&gt;
+            </span>
+          </li>
+          <li class="repo__list-item">
+            <span class="repo__label">github:</span>
+            <span class="repo__value repo__value-link">
+              &lt;<a
+                title="${name} - repository"
+                href="${html_url}"
+                target="_blank"
+                rel="noopener noreferrer"
+              >source code</a>&gt;
+            </span>
+          </li>
+        </ul>
+      </div>
+    </article>`;
 
       if(description) container.innerHTML += template;
 
